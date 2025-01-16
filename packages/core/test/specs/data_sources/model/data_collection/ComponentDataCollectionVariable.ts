cf774bb536fdd41bac5421b840506d79d62219fd
mohamedsalem401 <salemmohamed780@gmail.com>
2025-01-16 14:20:29 +0200

    Format

--- packages/core/test/specs/data_sources/model/data_collection/ComponentDataCollectionVariable.ts
+++ packages/core/test/specs/data_sources/model/data_collection/ComponentDataCollectionVariable.ts
@@ -105,7 +105,7 @@ describe('Collection variable components', () => {
         type: CollectionVariableType,
         variableType: DataCollectionStateVariableType.currentItem,
         path: 'user',
-      }
+      };
 
       const collectionComponentDefinition = {
         type: CollectionComponentType,
